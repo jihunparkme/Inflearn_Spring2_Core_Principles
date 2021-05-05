84085597c52888c483ff415a74835cbe7893b78e
jihunparkme <jihunpark.tech@gmail.com>
2021-05-05 22:01:04 +0900

    스프링으로 전환하기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,6 +9,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * AppConfig를 통해 관심사를 확실하게 분리 !
@@ -20,16 +22,19 @@ import hello.core.order.OrderServiceImpl;
  *
  * 관심사의 분리 : 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다 !@
  */
+@Configuration // 설정 Clss임을 Spring에게 알려주자
 public class AppConfig {
 
     /**
      * 생성한 객체 인스턴스의 참조(reference)를 생성자를 통해 주입 (생성자 주입)
      * DI, 의존관계 주입 : 의존관계를 외부에서 주입해주는 동작
      */
+    @Bean // Spring Container에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -38,11 +43,13 @@ public class AppConfig {
      * 메서드명을 보고 역할이 드러나도록 리팩터링
      * 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악
      */
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         /*
          * 어플리케이션의 구성 영역이 변경되더라도,
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,6 +4,8 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  *  순수 자바 코드로 Service Test
@@ -11,8 +13,18 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // appConfig에서 객체 가져오기
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // appConfig에서 객체 가져오기
+        /*
+         * ApplicationContext = Spring Container (@Bean을 관리)
+         *
+         * 스프링 컨테이너는 @Configuration 이 붙은 AppConfig 를 설정(구성) 정보로 사용
+         *  여기서 @Bean 이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록
+         * 이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈
+         */
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class); // Bean name, return Type
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
