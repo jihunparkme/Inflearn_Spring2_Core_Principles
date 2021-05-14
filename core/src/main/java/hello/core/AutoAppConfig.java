3cae2849b1ab0e67ba5c47b63798a5ed3e7d380f
jihunparkme <jihunpark.tech@gmail.com>
2021-05-14 22:55:56 +0900

    Component Scan 탐색 위치와 기본 스캔 대상

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -12,6 +12,12 @@ import org.springframework.context.annotation.FilterType;
  */
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", // 탐색할 패키지의 시작 위치 지정
+        basePackageClasses = AutoAppConfig.class, // 해당 class의 패키지를 탐색 시작 위치로
+        // default는 @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치
+        // 권장 방법 : 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 위치
+        // 사실 SpringBot를 사용하면 @SpringBootApplication에서 이미 ComponentScan을 하기 때문에 따로 해줄 필요는 없음
+
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // SpringBean 자동 등록에서 제외할 class 설정
         // ComponentScan을 하게되면 @Configuration으로 등록된 설정 정보도 함께 등록되므로, Configuration class는 제외해주자.
