2bb7d9f4a60e1e3a06b8625bd3e02e83acae12a5
jihunparkme <jihunpark.tech@gmail.com>
2021-05-27 23:20:32 +0900

    [빈 스코프] 스코프와 Provider

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,12 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider; //dependency lookup 할 수 있는 애가 주입
 
     @RequestMapping("log-demo")
     @ResponseBody // view가 아닌 문자 그대로 반환
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject(); //필요한 시점에 DL로부터  주입
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
